f80202c22f440fac74f107950235e51e25ea1cdb
luciana <lucianna.adm2010@gmail.com>
2022-03-08 23:17:15 -0300

    Initial project

---
+++ src/main/java/com/luciana/desafio/beer/mapper/BeerMapper.java
@@ -0,0 +1,17 @@
+package com.luciana.desafio.beer.mapper;
+
+import com.luciana.desafio.beer.dto.BeerDTO;
+import com.luciana.desafio.beer.entity.Beer;
+import pcom.luciana.desafio.beer.entity.Beer;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BeerMapper {
+
+    BeerMapper INSTANCE = Mappers.getMapper(BeerMapper.class);
+
+    Beer toModel(BeerDTO beerDTO);
+
+    BeerDTO toDTO(Beer beer);
+}
